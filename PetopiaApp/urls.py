dd8fcb9d262481907fc73384243f7f063cb471f3
Maja <maja.vuevska@gmail.com>
2023-07-01 22:17:12 +0200

    Final changes

--- PetopiaApp/urls.py
+++ PetopiaApp/urls.py
@@ -8,7 +8,7 @@ app_name = 'PetopiaApp'
 
 urlpatterns = [
     path("", home, name='home'),
-    path("<category>/", home, name='home'),
+    path("<category>", home, name='home'),
 
     path("add-pet-category/", add_pet_category, name='add_pet_category'),
     path("add-product/", add_product, name='add_product'),
@@ -18,7 +18,7 @@ urlpatterns = [
     path('cart/shopping-cart', shopping_cart, name='shopping_cart'),
     path('add-to-cart/<slug>', add_to_cart, name='add_to_cart'),
     path('remove-from-cart/<slug>', remove_from_cart, name='remove_from_cart'),
-    path('remove_one_item_from_cart/<slug>', remove_one_item_from_cart, name='remove_one_item_from_cart'),
+    path('remove-one-item-from-cart/<slug>', remove_one_item_from_cart, name='remove_one_item_from_cart'),
 
     path('checkout/', checkout, name='checkout'),
     path('payment/<payment_option>', payment, name='payment'),
